a652c24b368c690f24fe6881c3b67f0f18918f7f
wassim988 <57508814+wassim988@users.noreply.github.com>
2022-11-12 21:05:01 +0100

    Update OperateurController.java

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -22,7 +22,8 @@ public class OperateurController {
 	@GetMapping("/retrieve-all-operateurs")
 	@ResponseBody
 	public List<Operateur> getOperateurs() {
-		return operateurService.retrieveAllOperateurs();
+		List<Operateur> list = operateurService.retrieveAllOperateurs();
+		return list;
 	}
 
 	// http://localhost:8089/SpringMVC/operateur/retrieve-operateur/8
@@ -36,9 +37,11 @@ public class OperateurController {
 	@PostMapping("/add-operateur")
 	@ResponseBody
 	public Operateur addOperateur(@RequestBody Operateur op) {
-		return operateurService.addOperateur(op);
+		Operateur operateur = operateurService.addOperateur(op);
+		return operateur;
 	}
 
+	// http://localhost:8089/SpringMVC/operateur/remove-operateur/{operateur-id}
 	@DeleteMapping("/remove-operateur/{operateur-id}")
 	@ResponseBody
 	public void removeOperateur(@PathVariable("operateur-id") Long operateurId) {
