e9cb1dcf1ba589bdf0aba65c028192eaa9b40887
wassim988 <57508814+wassim988@users.noreply.github.com>
2022-11-12 21:15:56 +0100

    Update IFactureService.java

--- src/main/java/com/esprit/examen/services/IFactureService.java
+++ src/main/java/com/esprit/examen/services/IFactureService.java
@@ -14,8 +14,6 @@ public interface IFactureService {
 	void cancelFacture(Long id);
 
 	Facture retrieveFacture(Long id);
-	
-	void assignOperateurToFacture(Long idOperateur, Long idFacture);
 
 	float pourcentageRecouvrement(Date startDate, Date endDate);
 
