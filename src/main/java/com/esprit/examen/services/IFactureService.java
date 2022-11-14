610d8f3a118db898443cf68a4888e78a3e8a3ef7
nizarmahmoudi <48671951+nizarmahmoudi@users.noreply.github.com>
2022-11-14 21:04:40 +0100

    Add files via upload

---
+++ src/main/java/com/esprit/examen/services/IFactureService.java
@@ -0,0 +1,20 @@
+package com.esprit.examen.services;
+
+import java.util.Date;
+import java.util.List;
+import com.esprit.examen.entities.Facture;
+
+public interface IFactureService {
+	List<Facture> retrieveAllFactures();
+
+	List<Facture> getFacturesByFournisseur(Long idFournisseur);
+
+	Facture addFacture(Facture f);
+
+	void cancelFacture(Long id);
+
+	Facture retrieveFacture(Long id);
+	
+	float pourcentageRecouvrement(Date startDate, Date endDate);
+
+}
