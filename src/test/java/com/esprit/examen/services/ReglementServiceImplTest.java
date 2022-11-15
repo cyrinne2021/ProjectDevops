4db03edecd1eeb85b9709e645bf687ba591f73bb
abdallahouaili <73443971+abdallahouaili@users.noreply.github.com>
2022-11-15 01:28:21 +0100

    Update ReglementServiceImplTest.java

--- src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
@@ -8,13 +8,13 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
-@SpringBootTest
+
 class ReglementServiceImplTest {
 
     @Mock
